b0059cb632c6e1b3540b630d08941f8551b2eb65
yukselcodingwithyou <yuksel.ozdemir@dolap.com>
2021-04-18 12:20:43 +0300

    01 - init project

---
+++ src/main/java/com/yukselcoding/grpcrestbenchmark/GrpcRestBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package com.yukselcoding.grpcrestbenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcRestBenchmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcRestBenchmarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yukselcoding/grpcrestbenchmark/GrpcRestBenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yukselcoding.grpcrestbenchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcRestBenchmarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
